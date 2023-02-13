5c9b0297c1a20c46a531e38d129c71d8c512af44
Trang68 <trangdang6898@gmail.com>
2023-02-13 23:48:55 +0700

    day 2 chapter 2__8_9_100

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//1) 3ways to write a function
+  //1) 3ways to write a function
 
 //1- declare funtion by statment
 function sum(x,y) {
@@ -13,8 +13,8 @@ function sum(x,y) {
     return x+y;
   }
   
-  sumValue = summm(2,2);
-  console.log(sumValue); // err: summm is not defined 
+  // sumValue = summm(2,2);
+  // console.log(sumValue); // err: summm is not defined 
   //because sumValue will be hoisted, but the function declaration is not, so it will be undefined 
   
   //2.2 named function: 
@@ -39,8 +39,29 @@ function sum(x,y) {
     console.log('hi');
   }
    //2-2 expression: 
-  let goToWork = {RainIce?'No':'Yes'};
+   var exp = 1;
+   var salary = exp > 3 ? 1000 : 500;
+   console.log(salary) // 500
   
   
-   
-  
+
+//////////////////////day 2:
+//1/ 
+console.log('trang');
+console.error('this is an error');
+
+//  2. Assert
+let e;
+try {
+  const x = 3;
+  assert.equal(x, 8, 'x must be equal to 8')
+} catch (err) {
+  assert.equal(String(err),'AssertionError [ERR_ASSERTION]: x must be equal to 8');
+} 
+
+// console.assert.equal(3 + 5, 8);
+console.assert(5 > 7);
+
+
+var assert = require('assert');
+console.log(assert.equal(50, 50)); //OK
